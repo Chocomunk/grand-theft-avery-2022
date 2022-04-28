bd3b48ab6269b5ea5e67506727ebe89992224d9c
ashahidu <ashahidu@caltech.edu>
2022-04-28 15:55:01 -0700

    Basic gui

--- averyos/main.py
+++ averyos/main.py
@@ -1,11 +1,24 @@
 from shell.shell import Shell
 from puzzle.test_puzzle1 import test_puzzle1
+import tkinter as tk
+from terminal import Terminal
 
 
 if __name__ == '__main__':
-    root = test_puzzle1()
-    shell = Shell(root)
+    # root = test_puzzle1()
+    # shell = Shell(root)
 
-    running = True
-    while running:
-        running = shell.handle_input(input(shell.prompt()))
+    # running = True
+    # while running:
+    #     running = shell.handle_input(input(shell.prompt()))
+    root = tk.Tk()
+    terminal = Terminal(pady=5, padx=5, selectbackground='White')
+    terminal.pack(expand=True, fill='both')
+    # Don't want to run through our shell
+    # terminal.shell = True
+
+    terminal.basename = 'fuckyou$'
+
+
+
+    root.mainloop()
