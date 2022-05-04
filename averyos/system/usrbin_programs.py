b6a08567e1e63360a584ba12e2799515e6909060
Chocomunk <alvin.zhou.on@gmail.com>
2022-05-04 09:36:47 -0700

    Start password gui (IP)

--- averyos/system/usrbin_programs.py
+++ averyos/system/usrbin_programs.py
@@ -10,6 +10,7 @@ from gui.directory import DirectoryWidget
 from gui.labelbox import LabelBoxWidget
 from gui.password import PasswordWidget
 from gui.labelbox import LabelBoxWidget
+from gui.password import PasswordWidget
 
 
 EXIT_CMD = "exit"
