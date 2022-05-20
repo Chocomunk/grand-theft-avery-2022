61f24fa49348f09859d44be91fef9098d035e998
ashahidu <ashahidu@caltech.edu>
2022-05-20 00:42:45 -0700

    Add fanfic_files, add nodes, and lock epilogue

--- averyos/puzzle/dday_puzzle/fanfic/fanfic.py
+++ averyos/puzzle/dday_puzzle/fanfic/fanfic.py
@@ -12,9 +12,85 @@ from puzzle.dday_puzzle.programs.prompt_password import UnlockPromptPassword
 from .fanfic_files import *
 
 
+# These variables contain the ASCII art and text that appear when landing on
+# certain nodes.
+
+surprised_pikachu = \
+""" 
+⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿
+⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿
+⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿
+⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿
+"""
+
+shitpost_face = \
+""" 
+⠀⣞⢽⢪⢣⢣⢣⢫⡺⡵⣝⡮⣗⢷⢽⢽⢽⣮⡷⡽⣜⣜⢮⢺⣜⢷⢽⢝⡽⣝
+⠸⡸⠜⠕⠕⠁⢁⢇⢏⢽⢺⣪⡳⡝⣎⣏⢯⢞⡿⣟⣷⣳⢯⡷⣽⢽⢯⣳⣫⠇
+⠀⠀⢀⢀⢄⢬⢪⡪⡎⣆⡈⠚⠜⠕⠇⠗⠝⢕⢯⢫⣞⣯⣿⣻⡽⣏⢗⣗⠏⠀
+⠀⠪⡪⡪⣪⢪⢺⢸⢢⢓⢆⢤⢀⠀⠀⠀⠀⠈⢊⢞⡾⣿⡯⣏⢮⠷⠁⠀⠀
+⠀⠀⠀⠈⠊⠆⡃⠕⢕⢇⢇⢇⢇⢇⢏⢎⢎⢆⢄⠀⢑⣽⣿⢝⠲⠉⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⡿⠂⠠⠀⡇⢇⠕⢈⣀⠀⠁⠡⠣⡣⡫⣂⣿⠯⢪⠰⠂⠀⠀⠀⠀
+⠀⠀⠀⠀⡦⡙⡂⢀⢤⢣⠣⡈⣾⡃⠠⠄⠀⡄⢱⣌⣶⢏⢊⠂⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢝⡲⣜⡮⡏⢎⢌⢂⠙⠢⠐⢀⢘⢵⣽⣿⡿⠁⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠨⣺⡺⡕⡕⡱⡑⡆⡕⡅⡕⡜⡼⢽⡻⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣼⣳⣫⣾⣵⣗⡵⡱⡡⢣⢑⢕⢜⢕⡝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣴⣿⣾⣿⣿⣿⡿⡽⡑⢌⠪⡢⡣⣣⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⡟⡾⣿⢿⢿⢵⣽⣾⣼⣘⢸⢸⣞⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠁⠇⠡⠩⡫⢿⣝⡻⡮⣒⢽⠋⠀
+"""
+
+chalk_text = \
+""" 
+Looks like you sickos actually want to go through with this…
+"""
+
+drop_knees_text = \
+""" 
+I guess Katz’s heavenly presence really touched you, huh?
+"""
+
+expose_text = \
+""" 
+The fuck’s your obsession with Dinakar exposing himself, you creep?
+"""
+
+flirt_text = \
+""" 
+You sly dog ;)
+"""
+
+lets_begin_text = \
+""" 
+You might want to loosen your belt and untuck your shirt for what’s to come. It’s going to get hot in here…
+"""
+
+return_favor_text = \
+""" 
+Congratulations you pervert! You explored all possible paths in this godforsaken story, so here’s a little epilogue to satisfy you.
+"""
+
+last_thought_text = \
+""" 
+“Thank god men can’t get pregnant.”
+"""
+
+
 class FanficState:
 
     disclaimer_opened = False
+    # Unique integer index for each node and whether it has been visited
+    visited = {i: False for i in range(24)}
 
 
 def build_fanfic_graph():
@@ -26,10 +102,181 @@ def build_fanfic_graph():
         FanficState.disclaimer_opened = True
     disclaimer.add_open_callback(_disclaimer_open)
 
+    # Keep track of visited node index (as ordered in this file)
+    i = 0
+
+    # Visit node
+    def visit(i):
+        FanficState.visited[i] = True
+
     # begin
     begin = Node("begin-journey", parents=[root])
     begin.set_lock_func(lambda _: not FanficState.disclaimer_opened)
+    begin.directory.add_file(dinakars_lair)
+    begin.directory.add_file(whoareyou)
+    begin.directory.add_file(contract)
+    begin.add_entry_callback(lambda _: visit(i))
+    i += 1
 
+    # chalk
+    chalk = Node("bitch-about-chalk", parents=[begin])
+    chalk.directory.add_file(dinakars_strife)
+    chalk.directory.add_file(smug_voice)
+    chalk.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # continue_work
+    continue_work = Node("continue-working", parents=[chalk])
+    continue_work.directory.add_file(irresistible_temptation)
+    continue_work.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # respond
+    respond = Node("respond", parents=[chalk, continue_work])
+    respond.directory.add_file(questioning_figure)
+    respond.directory.add_file(legend_revealed)
+    respond.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # drop_knees
+    drop_knees = Node("drop-to-your-knees", parents=[respond])
+    drop_knees.directory.add_file(showing_reverence)
+    drop_knees.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # shock
+    shock = Node("stand-in-shock", parents=[respond, drop_knees])
+    shock.directory.add_file(katzs_admonishment)
+    shock.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # continue_standing
+    continue_standing = Node("continue-standing-still", parents=[shock])
+    continue_standing.directory.add_file(legend_approaches)
+    continue_standing.directory.add_file(legend_inquires)
+    continue_standing.directory.add_file(dinakars_feelings)
+    continue_standing.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # expose
+    expose = Node("expose-yourself", parents=[shock, continue_standing])
+    # TODO: Add files based on occurrences; write callback
+    expose.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # step_back
+    step_back = Node("step-back", parents=[continue_standing])
+    step_back.directory.add_file(attempted_retreat)
+    step_back.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # converse
+    converse = Node("converse", parents=[step_back])
+    converse.directory.add_file(revelation)
+    converse.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # flirt
+    flirt = Node("flirt", parents=[continue_standing, expose])
+    flirt.directory.add_file(dinakars_gamble)
+    flirt.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # get_boi
+    get_boi = Node("lets-get-this-boi", parents=[flirt])
+    get_boi.directory.add_file(pickup_line)
+    get_boi.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # reel_in
+    reel_in = Node("time-to-reel-him-in", parents=[get_boi])
+    reel_in.directory.add_file(hell)
+    reel_in.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # kiss
+    kiss = Node("kiss", parents=[step_back])
+    kiss.directory.add_file(dinakars_move)
+    kiss.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # lets_begin
+    lets_begin = Node("lets-begin", parents=[reel_in, converse])
+    lets_begin.directory.add_file(it_begins)
+    lets_begin.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # passion 
+    passion = Node("enter-the-throes-of-passion", parents=[lets_begin])
+    passion.directory.add_file(proposition)
+    passion.directory.add_file(beginning_of_love)
+    passion.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # intensify
+    intensify = Node("intensify", parents=[kiss, passion])
+    intensify.directory.add_file(french_kiss)
+    intensify.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # dragon
+    dragon = Node("release-the-dragon", parents=[intensify])
+    dragon.directory.add_file(exposure)
+    dragon.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # silent
+    silent = Node("remain-silent", parents=[dragon])
+    silent.directory.add_file(examination)
+    silent.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # boast
+    boast = Node("boast-about-your-size", parents=[dragon])
+    boast.directory.add_file(dinakars_tease)
+    boast.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # control
+    control = Node("let-nets-take-control", parents=[boast])
+    control.directory.add_file(nets_tease)
+    control.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # want
+    want = Node("tell-him-that-you-want-it", parents=[control, silent])
+    want.directory.add_file(dinakars_desire)
+    want.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # enter
+    enter = Node("enter-nets", parents=[want])
+    enter.directory.add_file(wide_mouth)
+    enter.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # climax 
+    climax = Node("climax", parents=[enter])
+    climax.directory.add_file(eruption)
+    climax.add_entry_callback(lambda _: visit(i))
+    i += 1
+
+    # finish
+    finish = Node("finish-up", parents=[climax])
+    finish.directory.add_file(resolution)
+
+    # return_favor
+    return_favour = Node("return-the-favor", parents=[climax])
+    return_favour.set_lock_func(lambda _: not all(FanficState.visited.values()))
+    return_favour.directory.add_file(continuation)
+    return_favour.directory.add_file(want_to_read_more)
+    return_favour.directory.add_file(too_far_to_stop)
+
+    # last_thought
+    last_thought = Node("last-thought", parents=[return_favour])
+
+    # end 
+    end = Node("end-story", parents=[finish, last_thought])
 
 
     return [root], None
--- averyos/puzzle/dday_puzzle/fanfic/fanfic_files.py
+++ averyos/puzzle/dday_puzzle/fanfic/fanfic_files.py
@@ -4,7 +4,8 @@ disclaimer = File("DO_NOT_OPEN.txt",
 """
 Andrew Ho brings you…Forbidden Mathematical Relations - A Caltech Story…
 Starring Dinakar Ramakrishnan and Nets Katz
-Please read text files in chronological order
+Please read text files in the order they are listed.
+If you feel uncomfortable at any time, there is no pressure to participate and let your alum know.
 """
 )
 
@@ -176,5 +177,225 @@ pick up the chalk fragments. Nets stooped down to help him out. They worked in s
 kept his eyes trained on the floor. As Dinakar felt his fingers brush Nets’, he slowly looked up to find Nets’ staring 
 right back at him, their noses joined by the tips. Dinakar looked away bashfully, but Nets made no movement, continuing 
 to stare directly at Dinakar. Neither made a sound for a moment.
+"""
+)
+
+dinakars_gamble = File("DINAKARS_GAMBLE.txt",
+"""
+Dinakar knew he couldn’t let this opportunity slip from his grasp, just as his cock would whenever he oiled it. As Dinakar 
+mustered the courage to confront the object of his desire, Nets could see into his eyes as if they were an open book. 
+Nets Katz knew what was coming, and countenancing the thought caused nothing less than a flush of arousal throughout his body.
+
+Dinakar began…
+"""
+)
+
+pickup_line = File("A_PICKUP_LINE.txt",
+"""
+“Did it hurt when you fell from heaven?” Dinakar said in a shaky voice. 
+
+Nets grimaced at first, but then realized Dinakar was new at this. He was still flowered. Nets would indulge this naivety, as 
+the reward was far too great, filling, and thick, like custard fresh from the baker’s shop. As he had lured many a professor 
+before, Nets knew exactly how to respond.
+
+“No, it hurts more when I crawl on my hands and knees out of hell,” Nets said stoicly. Dinakar was shocked. What could he possibly 
+mean? So Dinakar inquired, “What’s hell?” Katz reponded…
+"""
+)
+
+hell = File("HELL.txt",
+"""
+“Hell is life without you,” said a confident Nets Katz staring into Dinakar’s eyes. 
+
+The game was over. Dinakar played admirably and now knew what the two men had to do. As Nets fondly thought back to the day they 
+first met in the Teacher’s Lounge, it was time to begin…
+"""
+)
+
+revelation = File("A_REVELATION.txt",
+"""
+“You know, I’ve had my eye on you ever since I saw you in the Teacher’s Lounge on my first day,” whispered Nets.
+
+“What do you mean?”, asked Dinakar. His heart began to palpitate rapidly.
+
+“You know what I mean. We made eye contact that day. You can’t tell me that you don’t remember it,” Nets softly exclaimed. His 
+hand began to slide over Dinakar’s, sending powerful shivers down Dinakar’s spine. Dinakar remained silent, his mind racing with 
+confusion and lust. Was this some sort of prank? Some sort of cruel joke by the alluring Nets Katz to toy with his emotions?
+
+“I remember it like it was just yesterday,” continued Nets, “I saw you and thought to myself, holy mother of god. Look at that man’s 
+physique. Each and every feature, every curve, every fucking detail looks like it was carved by the god Michelangelo himself. A 
+masterpiece forged in the artistic pursuit to perfectly capture the purest essence and image of an Angel.”
+"""
+)
+
+dinakars_move = File("DINAKARS_MOVE.txt",
+"""
+As their tips touched, Dinakar knew he had to initiate, lest he lose the thing he wanted most. He eyed Nets lips, lips so soft and 
+supple they could be a scrotum. Dinakar just couldn’t resist the frothing of his loins. Dinakar moved his face an inch closer to Nets, 
+and began gently kissing him. Nets had done it, he had seduced Dinakar.
+
+They began to kiss more strongly, their passion increasingly exponentially by the second. Dinakar was a perceptive man, and had noticed 
+Nets’ impressive virility – his prominent erection clearly visible throughout their encounter. While Dinakar could not match the peak 
+performance of the legend Nets Katz, he could begin to feel his privates engorge. While not as powerful and complete as Nets, Dinakar 
+had an endowment that would make any man jealous.
+"""
+)
+
+it_begins = File("IT_BEGINS.txt",
+"""
+Nets sighed and wove his fingers between Dinakar’s.
+
+“I could only imagine the quiet, subtle power encased in the magnificent, yet tragically mortal vessel that was you Dinakar,” murmured 
+Nets. “The sensation that overcame me. The sensation that overpowered me was…almost palpable. All I knew from that day was that I wanted 
+you. No, more than that. I. Needed. You.”
+
+Nets’ hand had come to fully rest on Dinakar’s. Dinakar slowly shifted his gaze to meet Nets’. The flicker of the lamp seemed to make 
+Nets’ eyes dance with passion and lust, and Dinakar could feel his whole body flush with excitement.
+"""
+)
+
+proposition = File("A_PROPOSITION.txt",
+"""
+“Tell me, Di-Di. Do you feel how I feel?”
+
+Nets’ voice was barely audible, but his words lingered in the air.
+
+“Oh Nets. Just take me right now,” softly begged Dinakar.
+
+Nets smiled.
+"""
+)
+
+beginning_of_love = File("THE_BEGINNING_OF_LOVE.txt",
+"""
+“That’s all I’ve been waiting to hear,” whispered Nets. He leaned forward and Dinakar closed his eyes. Their lips met and they kissed 
+gingerly at first, then more aggressively as their hands began to run up and down each other’s bodies, groping and feeling as if they 
+were seeing one another touch for the first time, exploring new undiscovered territories, both physical and sexual.
+"""
+)
+
+french_kiss = File("A_FRENCH_KISS.txt",
+"""
+Nets swept away the chalk pieces strewn across the floor and grabbed Dinakar by the waist tenderly to lay him down, all the while 
+kissing his neck. Dinakar let out a soft moan as Nets’ scruff brushed against his chest, his body squirming in pleasure. Nets 
+repositioned himself above Dinakar, staring deeply into his eyes while caressing Dinakar’s face gently. Dinakar turned into Nets’ palm, 
+breathing in deeply.
+"""
+)
+
+exposure = File("EXPOSURE.txt",
+"""
+Dinakar’s eyes were still closed as he could feel Nets unbuttoning his shirt, exposing his hairy chest to the cold night air, his nipples 
+instantly erect at the frigidity of the room. Nets gently laid kiss after kiss down Dinakar’s sternum and stomach, eventually reaching 
+Dinakar’s belt. As Nets began to fiddle with the buckle, Dinakar suddenly reached down and grabbed his hands.
+
+“Are you sure?”, Dinakar whispered.
+
+Nets nodded and silently removed Dinakar’s grip and lightly dragged his hand down Dinakar’s front, sending him writhing in sensual 
+delight, his hands pressed flat against the floor as Nets yanked off Dinakar’s pants. Nets’ eyes shone brightly as he gingerly took 
+Dinakar’s semi-soft cock in his hand, weighing it like a scale.
+"""
+)
+
+examination = File("AN_EXAMINATION.txt",
+"""
+“Wow, you’re pretty big Dinakar,” gasped Nets. Dinakar said nothing and continued to wriggle on the floor in anticipation. Nets’ 
+grabbed Dinakar’s penis by the base and licked the head, causing it to instantly grow nearly two times in size and girth.
+
+“You like that?”, asked Nets, grinning widely.
+
+“Don’t tease me Nets,” panted Dinakar.
+"""
+)
+
+dinakars_tease = File("DINAKARS_TEASE.txt",
+"""
+Nets stared at Dinakar’s perfect specimen. He had never seen anything so beautiful before. Dinakar noticed and knew he had Nets Katz 
+wanting more. “You should know…I’m a grower,” Dinakar said with a newfound sense of confidence.
+
+Nets’ jaw dropped in astonishment. How could the magnificent tool he was holding and caressing in his hand be only the tip of the 
+iceberg? How could Dinakar have more to give?
+
+“Give it a lick Nets,” Dinakar stated.
+
+Nets needed to see if it was true, so he leaned down, adjusted his grip to insure Dinakar’s head was in an optimal position, and 
+extended his tongue. Dinakar groaned as his member extended as if it were a tape measure. Dinakar was not lying.
+"""
+)
+
+nets_tease = File("NETS_TEASE.txt",
+"""
+It was time for Nets Katz to show Dinakar why he was Caltech’s heartthrob, and to do so, he would make Dinakar’s penis throb. Nets 
+began licking faster and faster, as if Dinakar’s pecker were a lolly. Dinakar was in absolute ecstasy, but Nets knew he had to push 
+him further. He had to somehow make him want it even more.
+"""
+)
+
+dinakars_desire = File("DINAKARS_DESIRE.txt",
+"""
+“Mmm, I don’t think you want it badly enough. Tell me how badly you want it,” taunted Nets, beginning to slowly run his hands up and 
+down Dinakar’s meaty staff.
+
+“Please, I want it so bad just give it to me. Oh my god please don’t do this to me, I just want it so fucking bad,” Dinakar screamed.
+"""
+)
+
+wide_mouth = File("A_WIDE_MOUTH.txt",
+"""
+“That’s the spirit,” said Nets as he opened his mouth as widely as possible to take in Dinakar’s cock. Dinakar had never experience 
+pleasure so intense. Nets sucked up and down, all the while running circles around the head of his penis, occasionally stooping low to 
+tend to Dinakar’s testicles. Nets’ grip was tight yet not too tight, the perfect combination of softness and sensation, twisting and 
+shifting masterfully around Dinakar’s throbbing cock.
+"""
+)
+
+eruption = File("AN_ERUPTION.txt",
+"""
+“Holy shit I’m gonna cum. Holy FUCK!” screamed Dinakar. He let loose a volcanic fountain of white, hot sperm, startling and choking Nets 
+as it shot straight down his throat. Nets fell backwards and was hit in the face with another wave. Dinakar gasped in and out deeply, as 
+Nets removed his now soiled shirt.
+
+“Damn that wasn’t even a minute Dinakar. You gotta work on your stamina,” teased Nets, settling in next to Dinakar. Dinakar turned to 
+him and wiped some of his sweet semen off his shirt.
+"""
+)
+
+resolution = File("A_RESOLUTION.txt",
+"""
+“Let me return the favor,” whispered Dinakar, reaching for Nets’ impressive bulge, but Nets blocked his advance.
+
+“Nah, this way you owe me for next time,” said Nets, winking slyly at Dinakar.
+
+And there they remained lying on the floor, surrounded by chalk pieces, talking about everything and nothing until the morning sun began 
+to shine through the closed blinds.
+"""
+)
+
+continuation = File("THE_CONTINUATION.txt",
+"""
+“Let me return the favor,” whispered Dinakar, bending over on all fours to signal to Nets Katz that he was free to dock his own ship in 
+Dinakar Harbor. 
+
+Nets Katz undid his faded jeans and let a humungous worm of his own spring out, one that nearly rivalled Dinakar’s in dimensions. Nets 
+shuffled towards Dinakar’s orifice and performed the insertion. Dinakar couldn’t even process what he was feeling. Such pain from Nets’ 
+endowment, yet such stimulation from his mastery of the art. 
+"""
+)
+
+want_to_read_more = File("STILL_WANT_TO_READ_MORE.txt",
+"""
+Nets began to thrust deeper and deeper into Dinakar. This was the goal Nets Katz was working towards ever since he met Dinakar, and now 
+his fantasy had become reality. As the anus began to loosen to accommodate Nets, Nets’ penis continued to swell to take up the empty 
+space, as if Dinakar’s ass were a vacuum. Nets then felt the inevitable.
+"""
+)
+
+too_far_to_stop = File("I_GUESS_YOURE_IN_TOO_FAR_TO_STOP.txt",
+"""
+The dam broke. Nets began injecting his seed, one money shot after another, into Dinakar’s anus. Dinakar could feel Nets fill him with 
+his love, and loved every second of it. Seven, eight, nine. Dinakar kept counting the spurts, yet they did not decrease in volume and 
+power. Nets was not lying about stamina. Finally, after nearly twenty seconds of continuous ejaculation, Nets ceased.
+
+As Nets withdrew from Dinakar, he knew there was only one last thought that came to his mind.
 """
 )
